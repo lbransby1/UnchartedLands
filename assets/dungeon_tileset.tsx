ba5ebc465ef89c36c47458becbcae28a6df65d89
ec22472 <ec22472@qmul.ac.uk>
2024-01-22 20:34:17 +0000

    clean files

--- assets/dungeon_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="dungeon_tileset" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
- <image source="../gfx/gfx/cave.png" width="640" height="400"/>
-</tileset>
